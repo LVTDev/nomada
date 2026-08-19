3c98bbf4404710a0ca6c02db91d3b9056f7b381d
Vincent Botoku <desarrollador@grupolvt.com>
2026-08-19 11:50:12 -0600

    feat: adds privacidad page

---
+++ app/avisoDePrivacidad/page.tsx
@@ -0,0 +1,162 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="pt-12 pb-14 w-4/5 mx-auto">
+      <h1 className="font-bold text-center text-2xl">
+        AVISO DE PRIVACIDAD FESTIVAL INTERNACIONAL DE CINE DE MONTERREY A.C
+      </h1>
+      <p className="my-3">
+        El presente Aviso de Privacidad (aviso) se emite en cumplimiento por lo
+        dispuesto por el artículo 15 de la Ley Federal de Protección de Datos
+        Personales en Posesión de los Particulares (LFPDPPP), de los
+        Lineamientos Generales previstos en el artículo 43, en su fracción III
+        de esta Ley y del artículo 23 de su Reglamento.
+      </p>
+      <p className="mb-3">
+        Festival Internacional de Cine de Monterrey, A. C., como organizador y
+        dueño de NÓMADA FESTIVAL INTERNACIONAL DE CINE, tiene el compromiso
+        jurídico y social de cumplir con las medidas legales y de seguridad para
+        proteger sus datos personales con el objetivo de que usted tenga el
+        conocimiento, control y decisión sobre ellos y para las finalidades que
+        en el presente Aviso de Privacidad serán descritas.
+      </p>
+      <p className="mb-3">
+        Por ello, lo invitamos a que lea detenidamente la siguiente información:
+      </p>
+      <ol className="list-decimal">
+        <li className="mb-3">
+          <span className="font-bold">Datos del Responsable.{" "}{" "}</span>En su
+          artículo 3, fracción XIV, de la LFPDPPP establece que el Responsable
+          es: la; “Persona física o moral de carácter privado que decide sobre
+          el tratamiento de datos personales.” Festival Internacional de Cine de
+          Monterrey, A.C., como Responsable del tratamiento de Datos Personales
+          le comunica que es una Asociación Civil debidamente constituida de
+          conformidad con las leyes de los Estados Unidos Mexicanos con
+          domicilio en{" "}
+          <span className="font-bold">
+            Calle Río Rosas Sur No. 330, Interior 2A, Col. Del Valle, C.P.
+            66220, San Pedro Garza García, Nuevo León, México.
+          {" "}</span>
+        </li>
+        <li className="mb-3">
+          <span className="font-bold">Datos Personales.{" "}</span>De conformidad con
+          el artículo 3, fracción V, de la LFPDPPP son: “Datos personales:
+          Cualquier información concerniente a una persona física identificada o
+          identificable.” El Festival Internacional de Cine de Monterrey, A.C.,
+          recabará de usted los datos personales necesarios para el adecuado
+          cumplimiento de nuestro objeto social y la producción del festival,
+          este procedimiento puede ser de manera personal, directa o indirecta a
+          través de nuestros mecanismos establecidos. Estos Datos Personales
+          podrán incluir todos o algunos de los siguientes:
+        </li>
+        <li className="mb-3">
+          <span className="font-bold">Datos de identificación:{" "}</span> Nombre
+          completo, domicilio, correo electrónico, número de celular, Registro
+          Federal de Contribuyentes (RFC), Clave Única de Registro Poblacional
+          (CURP), lugar y fecha de nacimiento, edad.
+        </li>
+        <li className="mb-3">
+          <span className="font-bold">Datos laborales:{" "}</span>Ocupación, nombre
+          de la empresa, institución o dependencia, puesto, área o departamento,
+          domicilio, teléfono, correo electrónico de trabajo, referencias
+          laborales y referencias personales, giro de la empresa, institución o
+          dependencia.
+        </li>
+        <li className="mb-3">
+          <span className="font-bold">Datos financieros:{" "}</span>N° de cuenta
+          bancaria, CLABE, domicilio fiscal y nombre completo que usted utilice
+          para colaborar en la producción del festival al proporcionar estos
+          datos usted reconoce que está entregándonos su consentimiento expreso
+          para el tratamiento de estos datos.
+        </li>
+        <li className="mb-3">
+          <span className="font-bold">Datos sensibles:{" "}</span>De conformidad con
+          el artículo 3, fracción VI de la LFPDPPP, se entiende como Datos
+          personales sensibles: “Aquellos datos personales que afecten a la
+          esfera más íntima de su titular, o cuya utilización indebida pueda dar
+          origen a discriminación o conlleve un riesgo grave para éste. En
+          particular, se consideran sensibles aquellos que puedan revelar
+          aspectos como origen racial o étnico, estado de salud presente y
+          futuro, información genética, creencias religiosas, filosóficas y
+          morales, afiliación sindical, opiniones políticas, preferencia
+          sexual.”{" "}
+          <span className="font-bold">
+            {" "}
+            En este caso, el Festival Internacional de Cine de Monterrey, A.C.,
+            no recabará ni tratará datos personales sensibles.
+          {" "}</span>
+        </li>
+        <li className="mb-3">
+          <span className="font-bold">
+            Finalidades del Tratamiento de Datos.
+          {" "}</span>{" "}
+          Los Datos Personales obtenidos por Festival Internacional de Cine de
+          Monterrey, A. C., serán utilizados para la realización de actividades
+          culturales relacionadas con nuestro objeto social, dentro de ellas,
+          las siguientes: organización y desarrollo de{" "}
+          <span className="font-bold">
+            NÓMADA FESTIVAL INTERNACIONAL DE CINE,
+          {" "}</span>{" "}
+          compartir noticias como boletines y eventos especiales e información
+          de interés como participante del festival.
+        </li>
+        <li className="mb-3">
+          <span className="font-bold">Finalidades secundarias.{" "}</span>Además, si
+          usted no manifiesta lo contrario, el Festival Internacional de Cine de
+          Monterrey, A.C., tratará sus Datos Personales para las siguientes
+          finalidades secundarias: Contactarlo para cualquier tema relacionado
+          con el festival o con el presente Aviso de Privacidad. Efectuar
+          promociones, y publicidad respecto del festival. La atención y
+          procesamiento de solicitudes, la administraciónen actividades de
+          cobranza, aclaración, investigación y facturación de cargos
+          relacionados con el festival. Llevar a cabo consultas, investigaciones
+          y revisiones necesarias para la producción del festival y en relación
+          a sus quejas o peticiones.
+        </li>
+        <li className="mb-3">
+          <span className="font-bold">Transferencia de Datos Personales.{" "}</span>
+          Si usted no manifiesta lo contrario Festival Internacional de Cine de
+          Monterrey A.C., requiere transferir sus Datos Personales en la medida
+          que sea necesario para la producción del festival NÓMADA FESTIVAL
+          INTERNACIONAL DE CINE, por lo que se le informa que para poder
+          producir el festival, requerimos transferir dichos Datos Personales a:
+          Empresas o colaboradores bajo los mismos procesos y políticas de
+          respeto a la privacidad. Por lo que nos comprometemos a velar porque
+          se cumplan todos los principios legales de protección en torno a la
+          transferencia de sus datos personales.
+        </li>
+        <li className="mb-3">
+          <span className="font-bold">
+            Limitación en el Uso o Divulgación de los Datos Personales.
+          {" "}</span>{" "}
+          Usted puede limitar el uso o divulgación de sus datos personales para
+          dejar de recibir nuestra información y/o publicidad respecto al
+          festival y darse de baja a través de nuestros boletines electrónicos y
+          también puede contactarnos a este correo electrónico:
+          info@nomadafilmfestival.com y en la pagina web:
+          www.nomadafilmfestival.com . Trataremos sus Datos Personales única y
+          exclusivamente conforme a los términos del presente Aviso de
+          Privacidad.
+        </li>
+      </ol>
+      <p className="mb-3">
+        Usted, al proporcionar sus datos personales, confirma y acepta que ha
+        leído este Aviso de Privacidad y otorga su consentimiento para ajustarse
+        a lo señalado en el mismo.
+      </p>
+      <p className={"mb-3"}>
+        {" "}
+        Para cualquier solicitud relacionada con sus derechos ARCO, revocación
+        de consentimiento o dudas sobre este aviso, puede contactarnos a el
+        correo electrónico:info@nomadafilmfestival.com
+      </p>
+      <p className="mb-3">
+        Cualquier cambio o ajuste al presente se publicará en su momento
+        mediante la página web: www.nomadafilmfestival.com
+      </p>
+    </div>
+  );
+};
+
+export default page;
